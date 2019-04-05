2636ed1da79a97626872fc82aba8ca84553004d1
Brett Stainken <brettstainken.student@careerdevs.com>
2019-04-05 01:10:14 +0000

    corsanywhere

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 function search(){
     const keyword = document.getElementById("keyword").value;
     //alert (keyword)
-    
+    console.log(goodreadsApiKey);
     //Get results from API using fetch
     const endpoint = 'https://www.goodreads.com/search/index.xml'
+    const corsAnywhere = 'https://cors-anywhere.herokuapp.com/'
+    //url+ start of passing in parameters+APIkey+& identifies another paramenter + q as the keyword
+    const url = corsAnywhere + endpoint + "?" + "key=" + goodreadsApiKey + "&q=" + keyword
     
-    fetch()
+    fetch(url)
     //whatever fetch gets back is passed through the function with response as the Indep. Variable
     .then(function (response){
-        
+        console.log(response)
     })
     
     //display result as a list
