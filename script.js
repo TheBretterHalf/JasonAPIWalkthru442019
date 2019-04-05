d080f857c830487637bc03c7e0d67ca643efb44d
Brett Stainken <brettstainken.student@careerdevs.com>
2019-04-05 00:52:44 +0000

    adding

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function search(){
+    const keyword = document.getElementById("keyword").value;
+    //alert (keyword)
+    
+    //Get results from API using fetch
+    const endpoint = 'https://www.goodreads.com/search/index.xml'
+    
+    fetch()
+    //whatever fetch gets back is passed through the function with response as the Indep. Variable
+    .then(function (response){
+        
+    })
+    
+    //display result as a list
+    
+}
